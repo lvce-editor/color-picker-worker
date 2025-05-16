cf2538bdde8a7cf34ef05543d75423c69533e2dd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-16 23:07:24 +0200

    feature: add diff command (#6)
    * feature: add diff command
    
    * test
    
    * test

--- packages/color-picker-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/color-picker-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,6 @@
 import * as ColorPicker from '../ColorPicker/ColorPicker.ts'
 import * as Create from '../Create/Create.ts'
+import * as Diff2 from '../Diff2/Diff2.ts'
 import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
 import { handleSliderPointerDown } from '../HandleSliderPointerDown/HandleSliderPointerDown.ts'
 import { handleSliderPointerMove } from '../HandleSliderPointerMove/HandleSliderPointerMove.ts'
@@ -10,4 +11,5 @@ export const commandMap = {
   'ColorPicker.handleSliderPointerDown': handleSliderPointerDown,
   'ColorPicker.handleSliderPointerMove': handleSliderPointerMove,
   'ColorPicker.loadContent': ColorPicker.loadContent,
+  'ColorPicker.diff2': Diff2.diff2,
 }
