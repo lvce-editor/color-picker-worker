d303e44da7caf86b279096e4bb3e5b6b21169d0a
Le Vivilet <levivlet@pm.me>
2025-05-16 23:12:08 +0200

    wrap commands

--- packages/color-picker-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/color-picker-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,4 +1,5 @@
 import * as ColorPicker from '../ColorPicker/ColorPicker.ts'
+import * as WrapCommand from '../ColorPickerStates/ColorPickerStates.ts'
 import * as Create from '../Create/Create.ts'
 import * as Diff2 from '../Diff2/Diff2.ts'
 import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
@@ -11,8 +12,8 @@ export const commandMap = {
   'ColorPicker.create': Create.create,
   'ColorPicker.diff2': Diff2.diff2,
   'ColorPicker.getCommandIds': GetCommandIds.getCommandIds,
-  'ColorPicker.handleSliderPointerDown': handleSliderPointerDown,
-  'ColorPicker.handleSliderPointerMove': handleSliderPointerMove,
+  'ColorPicker.handleSliderPointerDown': WrapCommand.wrapCommand(handleSliderPointerDown),
+  'ColorPicker.handleSliderPointerMove': WrapCommand.wrapCommand(handleSliderPointerMove),
   'ColorPicker.loadContent': ColorPicker.loadContent,
   'ColorPicker.render2': Render2.render2,
   'ColorPicker.terminate': Terminate.terminate,
