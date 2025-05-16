9020c84b55399117fed6a87c2b3d6b89047eeb20
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-16 22:51:06 +0200

    feature: add color picker code (#2)
    * feature: add color picker code
    
    * simplify code
    
    * fix
    
    * return type

---
+++ packages/color-picker-worker/src/parts/GetColorPickerVirtualDom/GetColorPickerVirtualDom.ts
@@ -0,0 +1,46 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getColorPickerVirtualDom = (): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.ColorPicker),
+      onPointerDown: DomEventListenerFunctions.HandlePointerDown,
+      childCount: 3,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ColorPickerRectangle,
+      childCount: 3,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ColorPickerBackgroundColor,
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ColorPickerLight,
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ColorPickerDark,
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ColorPickerSlider,
+      childCount: 0,
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ColorPickerSliderThumb,
+      childCount: 0,
+    },
+  ]
+}
