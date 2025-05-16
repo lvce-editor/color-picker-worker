afd8dbb3404eee851b677c383fe1636b5356c270
Le Vivilet <levivlet@pm.me>
2025-05-16 22:50:21 +0200

    return type

--- packages/color-picker-worker/src/parts/GetColorPickerVirtualDom/GetColorPickerVirtualDom.ts
+++ packages/color-picker-worker/src/parts/GetColorPickerVirtualDom/GetColorPickerVirtualDom.ts
@@ -1,9 +1,10 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
-export const getColorPickerVirtualDom = () => {
+export const getColorPickerVirtualDom = (): readonly VirtualDomNode[] => {
   return [
     {
       type: VirtualDomElements.Div,
