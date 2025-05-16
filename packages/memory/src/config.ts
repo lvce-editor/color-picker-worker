395495a329d894dc0d18bdc6223b172d60be3803
Le Vivilet <levivlet@pm.me>
2025-05-16 22:44:12 +0200

    memory

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -3,6 +3,6 @@ import { root } from './root.ts'
 
 export const threshold = 400_000
 
-export const workerPath = join(root, '.tmp/dist/dist/iframeWorkerMain.js')
+export const workerPath = join(root, '.tmp/dist/dist/colorPickerWorkerMain.js')
 
 export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
