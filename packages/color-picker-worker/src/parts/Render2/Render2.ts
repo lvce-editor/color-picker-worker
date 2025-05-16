4a0a73007cb3b17695a66b4e8cd68ca8c465323c
Le Vivilet <levivlet@pm.me>
2025-05-16 23:12:23 +0200

    fix

--- packages/color-picker-worker/src/parts/Render2/Render2.ts
+++ packages/color-picker-worker/src/parts/Render2/Render2.ts
@@ -1,9 +1,9 @@
 import * as ApplyRender from '../ApplyRender/ApplyRender.ts'
-import * as SearchViewStates from '../SearchViewStates/SearchViewStates.ts'
+import * as ColorPickerStates from '../ColorPickerStates/ColorPickerStates.ts'
 
 export const render2 = (uid: number, diffResult: readonly number[]): readonly any[] => {
-  const { oldState, newState } = SearchViewStates.get(uid)
-  SearchViewStates.set(uid, newState, newState)
+  const { oldState, newState } = ColorPickerStates.get(uid)
+  ColorPickerStates.set(uid, newState, newState)
   const commands = ApplyRender.applyRender(oldState, newState, diffResult)
   return commands
 }
