7d3cead4edced4ff3bed65bdf103311e310d01ce
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-17 17:16:15 +0200

    feature: simplify color picker dom (#18)
    * feature: simplify color picker dom
    
    * test
    
    * test

--- packages/color-picker-worker/test/GetColorPickerVirtualDom.test.ts
+++ packages/color-picker-worker/test/GetColorPickerVirtualDom.test.ts
@@ -8,7 +8,6 @@ test('getColorPickerVirtualDom', () => {
     {
       type: VirtualDomElements.Div,
       className: 'Viewlet ColorPicker',
-      onPointerDown: 'handlePointerDown',
       childCount: 3,
     },
     {
@@ -34,6 +33,8 @@ test('getColorPickerVirtualDom', () => {
     {
       type: VirtualDomElements.Div,
       className: 'ColorPickerSlider',
+      onPointerDown: 'handleSliderPointerDown',
+      onPointerMove: 'handleSliderPointerMove',
       childCount: 0,
     },
     {
