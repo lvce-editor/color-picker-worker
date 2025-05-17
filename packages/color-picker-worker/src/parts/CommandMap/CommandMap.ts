78552aea1d142f85ccfefbbcdedcb47c4f09f91a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-17 17:00:57 +0200

    feature: compute bounds in loadContent function (#16)

--- packages/color-picker-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/color-picker-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,10 +1,10 @@
-import * as ColorPicker from '../ColorPicker/ColorPicker.ts'
 import * as WrapCommand from '../ColorPickerStates/ColorPickerStates.ts'
 import * as Create from '../Create/Create.ts'
 import * as Diff2 from '../Diff2/Diff2.ts'
 import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
 import { handleSliderPointerDown } from '../HandleSliderPointerDown/HandleSliderPointerDown.ts'
 import { handleSliderPointerMove } from '../HandleSliderPointerMove/HandleSliderPointerMove.ts'
+import * as ColorPicker from '../LoadContent/LoadContent.ts'
 import * as Render2 from '../Render2/Render2.ts'
 import * as Terminate from '../Terminate/Terminate.ts'
 
