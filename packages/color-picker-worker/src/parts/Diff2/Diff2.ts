6eee84cd1d449a3e17d844db13e5c0a74d89d671
Le Vivilet <levivlet@pm.me>
2025-05-17 12:07:55 +0200

    lint

--- packages/color-picker-worker/src/parts/Diff2/Diff2.ts
+++ packages/color-picker-worker/src/parts/Diff2/Diff2.ts
@@ -4,6 +4,5 @@ import * as Diff from '../Diff/Diff.ts'
 export const diff2 = (uid: number): readonly number[] => {
   const { oldState, newState } = ColorPickerStates.get(uid)
   const diffResult = Diff.diff(oldState, newState)
-  console.log({ oldState, newState, diffResult })
   return diffResult
 }
