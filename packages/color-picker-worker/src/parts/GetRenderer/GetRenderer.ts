4b31e10179c31c9093d6fd76cb5a7dcc034203a7
Le Vivilet <levivlet@pm.me>
2025-05-17 12:08:43 +0200

    rename

--- packages/color-picker-worker/src/parts/GetRenderer/GetRenderer.ts
+++ packages/color-picker-worker/src/parts/GetRenderer/GetRenderer.ts
@@ -1,20 +1,17 @@
-import type { Renderer } from '../Renderer/Renderer.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
 import * as RenderBounds from '../RenderBounds/RenderBounds.ts'
-import * as RenderColor from '../RenderColor/RenderColor.ts'
-import * as RenderOffsetX from '../RenderCss/RenderCss.ts'
+import * as RenderCss from '../RenderCss/RenderCss.ts'
+import type { Renderer } from '../Renderer/Renderer.ts'
 import * as RenderItems from '../RenderItems/RenderItems.ts'
 
 export const getRenderer = (diffType: number): Renderer => {
   switch (diffType) {
     case DiffType.RenderItems:
       return RenderItems.renderItems
-    case DiffType.RenderColor:
-      return RenderColor.renderColor
     case DiffType.RenderBounds:
       return RenderBounds.renderBounds
     case DiffType.RenderCss:
-      return RenderOffsetX.renderOffsetX
+      return RenderCss.renderCss
     default:
       throw new Error('unknown renderer')
   }
