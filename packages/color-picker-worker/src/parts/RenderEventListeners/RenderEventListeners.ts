a182101b2637a40f39c909f2e57537a6637033de
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-17 17:37:07 +0200

    feature: update rendereventlisteners (#21)
    * feature: update rendereventlistenrs
    
    * fix
    
    * log
    
    * lint

--- packages/color-picker-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
+++ packages/color-picker-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -1,7 +1,9 @@
+import type { ColorPickerState } from '../ColorPickerState/ColorPickerState.ts'
 import { getEventListeners } from '../GetEventListeners/GetEventListeners.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
 
-export const renderEventListeners = (): readonly any[] => {
+export const renderEventListeners = (state: ColorPickerState): readonly any[] => {
+  const { uid } = state
   const eventListeners = getEventListeners()
-  return [RenderMethod.SetEventListeners, eventListeners]
+  return [RenderMethod.SetEventListeners, uid, eventListeners]
 }
