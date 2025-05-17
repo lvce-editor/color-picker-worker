0724fa148a1cc3276aa368c91cc9ae3666cba2b3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-17 20:04:15 +0200

    feature: pass widget id to event listeners (#24)

--- packages/color-picker-worker/src/parts/GetEventListeners/GetEventListeners.ts
+++ packages/color-picker-worker/src/parts/GetEventListeners/GetEventListeners.ts
@@ -1,15 +1,15 @@
 import type { DomEventListener } from '../DomEventListener/DomEventListener.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
-export const getEventListeners = (): readonly DomEventListener[] => {
+export const getEventListeners = (uid: number, widgetId: number): readonly DomEventListener[] => {
   return [
     {
       name: DomEventListenerFunctions.HandleSliderPointerDown,
-      params: ['handleSliderPointerDown', 'event.clientX', 'event.clientY'],
+      params: ['executeWidgetCommand', 'ColorPicker', 'handleSliderPointerDown', uid, widgetId, 'event.clientX', 'event.clientY'],
     },
     {
       name: DomEventListenerFunctions.HandleSliderPointerMove,
-      params: ['handleSliderPointerMove', 'event.clientX', 'event.clientY'],
+      params: ['executeWidgetCommand', 'ColorPicker', 'handleSliderPointerMove', uid, widgetId, 'event.clientX', 'event.clientY'],
     },
   ]
 }
--- packages/color-picker-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
+++ packages/color-picker-worker/src/parts/RenderEventListeners/RenderEventListeners.ts
@@ -1,9 +1,10 @@
 import type { ColorPickerState } from '../ColorPickerState/ColorPickerState.ts'
 import { getEventListeners } from '../GetEventListeners/GetEventListeners.ts'
 import * as RenderMethod from '../RenderMethod/RenderMethod.ts'
+import * as WidgetId from '../WidgetId/WidgetId.ts'
 
 export const renderEventListeners = (state: ColorPickerState): readonly any[] => {
   const { uid } = state
-  const eventListeners = getEventListeners()
+  const eventListeners = getEventListeners(uid, WidgetId.ColorPicker)
   return [RenderMethod.SetEventListeners, uid, eventListeners]
 }
