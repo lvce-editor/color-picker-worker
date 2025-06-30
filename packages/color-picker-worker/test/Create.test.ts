e269147778c85e488336647f2ac8b683d2c91273
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-30 23:25:41 +0200

    Discard changes to packages/color-picker-worker/test/Create.test.ts

--- packages/color-picker-worker/test/Create.test.ts
+++ packages/color-picker-worker/test/Create.test.ts
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import { test, expect } from '@jest/globals'
-import { create } from '../src/parts/Create/Create.js'
-
-test('create returns expected result', () => {
-  const result = create(1, 0, 0, 100, 100)
-  expect(result).toEqual({
-    uid: 1,
-    color: '',
-    height: 100,
-    max: 0,
-    min: 0,
-    offsetX: 0,
-    width: 0,
-    x: 0,
-    y: 0,
-=======
 import { expect, test } from '@jest/globals'
 import { create } from '../src/parts/Create/Create.ts'
 
@@ -40,6 +23,5 @@ test('create - basic', () => {
     y,
     version: 0,
     parentUid,
->>>>>>> origin/main
   })
 })
