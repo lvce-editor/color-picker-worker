cab24254cf4e78173f0c1a989d96a507e24ff3dd
Le Vivilet <levivlet@pm.me>
2025-06-30 23:53:21 +0200

    feature: use namespace import

--- packages/color-picker-worker/src/parts/HandleSliderPointerDown/HandleSliderPointerDown.ts
+++ packages/color-picker-worker/src/parts/HandleSliderPointerDown/HandleSliderPointerDown.ts
@@ -1,15 +1,15 @@
 import type { ColorPickerState } from '../ColorPickerState/ColorPickerState.ts'
 import * as Clamp from '../Clamp/Clamp.ts'
-import { getNewColor } from '../GetNewColor/GetNewColor.ts'
+import * as GetNewColor from '../GetNewColor/GetNewColor.ts'
 
 export const handleSliderPointerDown = (state: ColorPickerState, eventX: number, eventY: number): ColorPickerState => {
   const { min, max, x } = state
   const relativeX = eventX - x
   const newX = Clamp.clamp(relativeX, min, max)
-  const newColor = getNewColor(newX, max)
+  const newcolor = GetNewColor.getNewColor(newX, max)
   return {
     ...state,
-    color: newColor,
+    color: newcolor,
     offsetX: newX,
   }
 }
