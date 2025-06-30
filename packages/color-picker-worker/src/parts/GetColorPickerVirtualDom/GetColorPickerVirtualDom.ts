c683697fed57b96b1c17f26ee69a86cf2523ae67
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-30 23:51:52 +0200

    feature: use namespace import (#51)

--- packages/color-picker-worker/src/parts/GetColorPickerVirtualDom/GetColorPickerVirtualDom.ts
+++ packages/color-picker-worker/src/parts/GetColorPickerVirtualDom/GetColorPickerVirtualDom.ts
@@ -1,8 +1,8 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import { getColorPickerRectangleVirtualDom } from '../GetColorPickerRectangleVirtualDom/GetColorPickerRectangleVirtualDom.ts'
-import { getColorPickerSliderThumbVirtualDom } from '../GetColorPickerSliderThumbVirtualDom/GetColorPickerSliderThumbVirtualDom.ts'
-import { getColorPickerSliderVirtualDom } from '../GetColorPickerSliderVirtualDom/GetColorPickerSliderVirtualDom.ts'
+import * as GetColorPickerRectangleVirtualDom from '../GetColorPickerRectangleVirtualDom/GetColorPickerRectangleVirtualDom.ts'
+import * as GetColorPickerSliderThumbVirtualDom from '../GetColorPickerSliderThumbVirtualDom/GetColorPickerSliderThumbVirtualDom.ts'
+import * as GetColorPickerSliderVirtualDom from '../GetColorPickerSliderVirtualDom/GetColorPickerSliderVirtualDom.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
@@ -13,5 +13,5 @@ const parentNode: VirtualDomNode = {
 }
 
 export const getColorPickerVirtualDom = (): readonly VirtualDomNode[] => {
-  return [parentNode, ...getColorPickerRectangleVirtualDom(), ...getColorPickerSliderVirtualDom(), ...getColorPickerSliderThumbVirtualDom()]
+  return [parentNode, ...GetColorPickerRectangleVirtualDom.getColorPickerRectangleVirtualDom(), ...GetColorPickerSliderVirtualDom.getColorPickerSliderVirtualDom(), ...GetColorPickerSliderThumbVirtualDom.getColorPickerSliderThumbVirtualDom()]
 }
