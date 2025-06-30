cd65919207f4d772b5b8c9e18e4aa7b16fa0db2a
Le Vivilet <levivlet@pm.me>
2025-06-30 23:27:31 +0200

    lint

--- packages/color-picker-worker/test/DiffColor.test.ts
+++ packages/color-picker-worker/test/DiffColor.test.ts
@@ -1,21 +1,6 @@
 import { test, expect } from '@jest/globals'
 import { isEqual } from '../src/parts/DiffColor/DiffColor.js'
-
-// Mock implementation of createDefaultState
-const createDefaultState = () => ({
-  color: '#000000',
-  offsetX: 0,
-  max: 100,
-  uid: 1,
-  min: 0,
-  x: 0,
-  y: 0,
-  width: 100,
-  height: 100,
-  items: [],
-  bounds: { x: 0, y: 0, width: 0, height: 0 },
-  modules: [],
-})
+import { createDefaultState } from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 
 test('isEqual returns true when states are equal', () => {
   const oldState = createDefaultState()
