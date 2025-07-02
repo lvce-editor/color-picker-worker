8811fb184977e2270d7ea53ab2c80a23ae715209
Le Vivilet <levivlet@pm.me>
2025-07-02 18:04:55 +0200

    feature: simplify terminate command

--- packages/color-picker-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/color-picker-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,3 +1,4 @@
+import { terminate } from '@lvce-editor/viewlet-registry'
 import * as WrapCommand from '../ColorPickerStates/ColorPickerStates.ts'
 import * as Create from '../Create/Create.ts'
 import * as Diff2 from '../Diff2/Diff2.ts'
@@ -7,7 +8,6 @@ import { handleSliderPointerDown } from '../HandleSliderPointerDown/HandleSlider
 import { handleSliderPointerMove } from '../HandleSliderPointerMove/HandleSliderPointerMove.ts'
 import * as ColorPicker from '../LoadContent/LoadContent.ts'
 import * as Render2 from '../Render2/Render2.ts'
-import * as Terminate from '../Terminate/Terminate.ts'
 
 export const commandMap = {
   'ColorPicker.create': Create.create,
@@ -17,6 +17,6 @@ export const commandMap = {
   'ColorPicker.handleSliderPointerMove': WrapCommand.wrapCommand(handleSliderPointerMove),
   'ColorPicker.loadContent': WrapCommand.wrapCommand(ColorPicker.loadContent),
   'ColorPicker.render2': Render2.render2,
-  'ColorPicker.terminate': Terminate.terminate,
+  'ColorPicker.terminate': terminate,
   'ColorPicker.dispose': Dispose.dispose,
 }
--- packages/color-picker-worker/test/Terminate.test.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect, jest } from '@jest/globals'
-import * as Terminate from '../src/parts/Terminate/Terminate.ts'
-
-test('terminate', () => {
-  const mockClose: jest.Mock = jest.fn()
-  globalThis.close = mockClose
-  Terminate.terminate()
-  expect(mockClose).toHaveBeenCalled()
-})
