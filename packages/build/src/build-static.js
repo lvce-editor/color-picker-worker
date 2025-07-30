d02553d54555a6bbb96ae63327951b426159a3f5
Le Vivilet <levivlet@pm.me>
2025-07-30 11:42:17 +0200

    feature: update color picker worker url in postinstall

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -1,7 +1,7 @@
+import { cp, readFile, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { pathToFileURL } from 'node:url'
 import { root } from './root.js'
-import { cp } from 'node:fs/promises'
 
 const sharedProcessPath = join(root, 'packages', 'server', 'node_modules', '@lvce-editor', 'shared-process', 'index.js')
 
@@ -10,14 +10,29 @@ const sharedProcessUrl = pathToFileURL(sharedProcessPath).toString()
 const sharedProcess = await import(sharedProcessUrl)
 
 process.env.PATH_PREFIX = '/color-picker-worker'
-await sharedProcess.exportStatic({
+const { commitHash } = await sharedProcess.exportStatic({
   root,
   extensionPath: '',
 })
 
-// await cp(
-//   join(root, '.tmp', 'dist', 'dist', 'iframeWorkerMain.js'),
-//   join(root, 'dist', commitHash, 'packages', 'iframe-worker', 'dist', 'iframeWorkerMain.js'),
-// )
+const rendererWorkerPath = join(root, 'dist', commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
+
+export const getRemoteUrl = (path) => {
+  const url = pathToFileURL(path).toString().slice(8)
+  return `/remote/${url}`
+}
+
+const content = await readFile(rendererWorkerPath, 'utf8')
+const workerPath = join(root, '.tmp/dist/dist/colorPickerWorkerMain.js')
+const remoteUrl = getRemoteUrl(workerPath)
+
+const occurrence = `// const colorPickerWorkerUrl = \`\${assetDir}/packages/color-picker-worker/dist/colorPickerWorkerMain.js\`
+const colorPickerWorkerUrl = \`${remoteUrl}\``
+const replacement = `const colorPickerWorkerUrl = \`\${assetDir}/packages/color-picker-worker/dist/colorPickerWorkerMain.js\``
+if (!content.includes(occurrence)) {
+  throw new Error('occurrence not found')
+}
+const newContent = content.replace(occurrence, replacement)
+await writeFile(rendererWorkerPath, newContent)
 
 await cp(join(root, 'dist'), join(root, '.tmp', 'static'), { recursive: true })
--- packages/server/src/postinstall.js
+++ packages/server/src/postinstall.js
@@ -1,1 +1,39 @@
-export {}
+import { readFile, readdir, writeFile } from 'node:fs/promises'
+import { join } from 'node:path'
+import { pathToFileURL } from 'node:url'
+
+const __dirname = import.meta.dirname
+
+const root = join(__dirname, '..', '..', '..')
+
+export const getRemoteUrl = (path) => {
+  const url = pathToFileURL(path).toString().slice(8)
+  return `/remote/${url}`
+}
+
+const nodeModulesPath = join(root, 'packages', 'server', 'node_modules')
+
+const workerPath = join(root, '.tmp', 'dist', 'dist', 'colorPickerWorkerMain.js')
+
+const serverStaticPath = join(nodeModulesPath, '@lvce-editor', 'static-server', 'static')
+
+const RE_COMMIT_HASH = /^[a-z\d]+$/
+const isCommitHash = (dirent) => {
+  return dirent.length === 7 && dirent.match(RE_COMMIT_HASH)
+}
+
+const dirents = await readdir(serverStaticPath)
+const commitHash = dirents.find(isCommitHash) || ''
+const rendererWorkerMainPath = join(serverStaticPath, commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
+
+const content = await readFile(rendererWorkerMainPath, 'utf-8')
+
+const remoteUrl = getRemoteUrl(workerPath)
+if (!content.includes('// const colorPickerWorkerUrl = ')) {
+  const occurrence = `const colorPickerWorkerUrl = \`\${assetDir}/packages/color-picker-worker/dist/colorPickerWorkerMain.js\``
+  const replacement = `// const colorPickerWorkerUrl = \`\${assetDir}/packages/color-picker-worker/dist/colorPickerWorkerMain.js\`
+const colorPickerWorkerUrl = \`${remoteUrl}\``
+
+  const newContent = content.replace(occurrence, replacement)
+  await writeFile(rendererWorkerMainPath, newContent)
+}
