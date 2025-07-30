b30241877536fa0653fb51e502af11d9b6ea1890
Le Vivilet <levivlet@pm.me>
2025-07-30 15:10:09 +0200

    feature: remove unused code

--- packages/color-picker-worker/test/GetCommandIds.test.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@jest/globals'
-import * as GetCommandIds from '../src/parts/GetCommandIds/GetCommandIds.ts'
-
-test('getCommandIds', () => {
-  const result: readonly string[] = GetCommandIds.getCommandIds()
-  expect(result).toBeDefined()
-})
