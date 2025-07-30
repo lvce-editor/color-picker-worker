62f6c20e131fb612045a2b0170b04a5664ea356a
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-30 14:59:23 +0200

    feature: simplify command ids (#80)

--- packages/color-picker-worker/src/parts/ColorPickerStates/ColorPickerStates.ts
+++ packages/color-picker-worker/src/parts/ColorPickerStates/ColorPickerStates.ts
@@ -1,4 +1,4 @@
 import * as ViewletRegistry from '@lvce-editor/viewlet-registry'
 import type { ColorPickerState } from '../ColorPickerState/ColorPickerState.ts'
 
-export const { get, set, wrapCommand, dispose } = ViewletRegistry.create<ColorPickerState>()
+export const { get, set, wrapCommand, dispose, getCommandIds, registerCommands } = ViewletRegistry.create<ColorPickerState>()
--- packages/color-picker-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/color-picker-worker/src/parts/CommandMap/CommandMap.ts
@@ -3,7 +3,6 @@ import * as WrapCommand from '../ColorPickerStates/ColorPickerStates.ts'
 import * as Create from '../Create/Create.ts'
 import * as Diff2 from '../Diff2/Diff2.ts'
 import * as Dispose from '../Dispose/Dispose.ts'
-import * as GetCommandIds from '../GetCommandIds/GetCommandIds.ts'
 import { handleSliderPointerDown } from '../HandleSliderPointerDown/HandleSliderPointerDown.ts'
 import { handleSliderPointerMove } from '../HandleSliderPointerMove/HandleSliderPointerMove.ts'
 import { handleSliderPointerUp } from '../HandleSliderPointerUp/HandleSliderPointerUp.ts'
@@ -13,7 +12,7 @@ import * as Render2 from '../Render2/Render2.ts'
 export const commandMap = {
   'ColorPicker.create': Create.create,
   'ColorPicker.diff2': Diff2.diff2,
-  'ColorPicker.getCommandIds': GetCommandIds.getCommandIds,
+  'ColorPicker.getCommandIds': WrapCommand.getCommandIds,
   'ColorPicker.handleSliderPointerDown': WrapCommand.wrapCommand(handleSliderPointerDown),
   'ColorPicker.handleSliderPointerMove': WrapCommand.wrapCommand(handleSliderPointerMove),
   'ColorPicker.handleSliderPointerUp': WrapCommand.wrapCommand(handleSliderPointerUp),
